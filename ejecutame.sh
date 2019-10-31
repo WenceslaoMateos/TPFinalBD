646c404c6bb83d9b43292eac55c2cf36a976bbaa
Wenceslao <wenceslaomateos@gmail.com>
2019-10-31 11:14:16 -0300

    Agregado de un archivo para carga facil

---
+++ ejecutame.sh
@@ -0,0 +1,4 @@
+# Para ejecutar el archivo completo, usar
+mysql -u"wence" -p"wence" -e "DROP DATABASE test_TP;"
+mysql -u"wence" -p"wence" -e "CREATE DATABASE test_TP;"
+mysql -u"wence" -p"wence" test_TP < BD.sql
