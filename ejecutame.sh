d42654fd9c0771d80cf0ee09c2f007a21b71eb87
Wenceslao <wenceslaomateos@gmail.com>
2019-10-31 11:17:45 -0300

    Archivo innecesario

--- ejecutame.sh
+++
@@ -1,4 +0,0 @@
-# Para ejecutar el archivo completo, usar
-mysql -u"wence" -p"wence" -e "DROP DATABASE test_TP;"
-mysql -u"wence" -p"wence" -e "CREATE DATABASE test_TP;"
-mysql -u"wence" -p"wence" test_TP < BD.sql
