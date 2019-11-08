8a3134cab38d612cf5fb21a0a81fcaf303b4db59
Wenceslao <wenceslaomateos@gmail.com>
2019-11-08 16:57:06 -0300

    Agregado de algunos triggers

--- BD.sql
+++ BD.sql
@@ -172,6 +172,202 @@ CREATE TABLE Capacitado_para(
     FOREIGN KEY(cod_actividad) REFERENCES Actividad(cod_actividad) ON DELETE CASCADE
 );
 
+/* 
+ * Seccion de triggers para modelar restricciones
+ */
+
+/*
+ * Dada una instancia de TITULAR, el valor correspondiente a TITULAR.id_categoria que se iguala con 
+ * CATEGORIA.id_categoria, esta instancia de CATEGORÍA, el atributo CATEGORÍA.descripción sólo puede los 
+ * valores “mayor” o “vitalicio”.
+ */
+delimiter $$ 
+CREATE TRIGGER titular_categoria 
+BEFORE INSERT ON Titular 
+FOR EACH ROW 
+BEGIN 
+    DECLARE dummy varchar(20);
+    SELECT
+        c.descripcion
+    INTO
+        dummy
+    FROM
+        Categoria c
+    WHERE
+      c.id_categoria = NEW.id_categoria;
+    if (dummy <> 'vitalicio') AND (dummy <> 'mayor') then
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'No se puede insertar un Titular con una categoría que sea distinta de Mayor o Vitalicio';
+    end if;
+END $$
+
+/*
+ * Creacion del id del familiar cuando se agrega
+ */
+CREATE TRIGGER familiar_nro_orden 
+BEFORE INSERT ON Familiar 
+FOR EACH ROW 
+BEGIN 
+    DECLARE dummy int;
+    SELECT
+        count(f.nro_orden)
+    INTO
+        dummy
+    FROM
+        Familiar f
+    WHERE
+      f.nro_socio = NEW.nro_socio;
+    SET dummy = dummy+1;
+    SET NEW.nro_orden = CONCAT('orden', dummy);
+END $$
+
+/*
+ * ACTIVIDAD.cod_actividad solo puede estar en CLASE.cod_actividad si para la instancia correspondiente 
+ * en PUEDE-DESARROLLARSE-EN, dada por la igualdad entre ACTIVIDAD.cod_actididad y ÁREA.cod_area, los 
+ * valores de ÁREA.cod_area y CLASE.cod_area son iguales.
+ */
+delimiter $$ 
+CREATE TRIGGER clase_en_area 
+BEFORE INSERT ON Clase 
+FOR EACH ROW 
+BEGIN 
+    DECLARE dummy int;
+    SELECT
+        count(*)
+    INTO
+        dummy
+    FROM
+        Clase c, Actividad ac, Puede_Desarrollarse_En pde, Area ar
+    WHERE
+      c.cod_actividad = ac.cod_actividad AND
+      ac.cod_actividad = pde.cod_actividad AND
+      pde.cod_area = ar.cod_area AND
+      ar.cod_area = c.cod_area;
+    if dummy = 0 then
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'No puede existir una clase con una actividad que no se pueda desarrollar en ese area';
+    end if;
+END $$
+
+/*
+ * TITULAR.nro_socio sólo puede estar en PAGA_T.nro_socio si para la instancia correspondiente en 
+ * SE_INSCRIBE_T, dada por la igualdad entre SE_INSCRIBE_T.id_clase y CLASE.id_clase, y para la instancia 
+ * de CLASE, dada por la igualdad entre CLASE.cod_actividad y ACTIVIDAD.cod_actividad, el atributo 
+ * ACTIVIDAD.arancelada posee el valor "true"
+ */
+delimiter $$ 
+CREATE TRIGGER titular_paga_clase 
+BEFORE INSERT ON Paga_t 
+FOR EACH ROW 
+BEGIN 
+    DECLARE dummy varchar(20);
+    SELECT
+        count(*)
+    INTO
+        dummy
+    FROM
+        Titular t, Se_Inscribe_t st, Clase c, Actividad a
+    WHERE
+      t.nro_socio = st.nro_socio AND
+      st.id_clase = c.id_clase AND
+      c.cod_actividad = a.cod_actividad AND
+      a.tipo_costo = true;
+    if dummy = 0 then
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'No puede existir una clase con una actividad que no se pueda desarrollar en ese area';
+    end if;
+END $$
+
+/*
+ * (FAMILIAR.nro_socio, FAMILIAR.nro_orden) sólo puede estar en (PAGA_F.nro_socio, PAGA_F.nro_orden) 
+ * si para la instancia correspondiente en SE_INSCRIBE_F, dada por la igualdad entre SE_INSCRIBE_F.id_clase 
+ * y CLASE.id_clase, y para la instancia de CLASE, dada por la igualdad entre CLASE.cod_actividad y 
+ * ACTIVIDAD.cod_actividad, el atributo ACTIVIDAD.tipo_costo posee el valor “arancelada”
+ */
+
+/*
+ * Dada una instancia de PROFESIONAL.legajo que está en DIRIGE.legajo y además DIRIGE.id_clase en 
+ * CLASE.id_clase, y también una instancia de A-CARGO-DE.legajo igual a PROFESIONAL.legajo, 
+ * A-CARGO-DE.cod_actividad debe ser igual a CLASE.cod_actividad
+ */
+
+/*
+ * El dominio del atributo ACTIVIDAD.tipo_costo solo puede tomar los valores “arancelada” o “gratuita”
+ */
+
+/*
+ * TITULAR.nro_socio sólo puede estar en SE-INSCRIBE.nro_socio si para la instancia 
+ * correspondiente en SE-INSCRIBE, dada por la igualdad entre SE-INSCRIBE.id_clase y CLASE.id_clase, 
+ * y para la instancia de CLASE, dada por la igualdad entre CLASE.cod_actividad y ACTIVIDAD.cod_actividad, 
+ * TITULAR.id_categoria es igual a ACTIVIDAD.id_categoría
+ */
+
+/*
+ * (FAMILIAR.nro_socio,FAMILIAR.nro_orden) sólo puede estar en (SE-INSCRIBE.nro_socio, 
+ * SE-INSCRIBE.nro_orden) si para la instancia correspondiente en SE-INSCRIBE, dada por la igualdad 
+ * entre SE-INSCRIBE.id_clase y CLASE.id_clase, y para la instancia de CLASE, dada por la igualdad entre 
+ * CLASE.cod_actividad y ACTIVIDAD.cod_actividad, FAMILIAR.id_categoria es igual a ACTIVIDAD.id_categoría
+ */
+
+delimiter ;
+
+/*
+ * INSERCIONES
+ */
+INSERT INTO
+    Categoria(id_categoria, descripcion, porcentaje)
+VALUES
+    ('cat001', 'infantil', '0.15'),
+    ('cat002', 'mayor', '0.2'),
+    ('cat003', 'vitalicio', '0');
+
+INSERT INTO 
+    Titular(
+      nro_socio,
+      id_categoria,
+      nombre,
+      apellido,
+      fecha_nac,
+      email,
+      celular,
+      telefono,
+      domicilio
+    )
+VALUES
+    (
+        'soc001', 
+        'cat002', 
+        'Martin', 
+        'Belcic', 
+        '1996-11-01', 
+        'martin.belcic@gmail.com', 
+        '2236888888', 
+        '4723339',
+        'Alberti 2565'
+    );
+
+INSERT INTO 
+    Familiar(
+      nro_socio,
+      id_categoria,
+      nombre,
+      apellido,
+      fecha_nac,
+      email,
+      celular
+    )
+VALUES
+    (
+        'soc001', 
+        'cat001', 
+        'Martin', 
+        'Belcic', 
+        '1996-11-01', 
+        'martin.belcic@gmail.com', 
+        '2236888888'
+    );
+
+/* Profesional esta todo mal
 _______________________________________________________________________________________________________________________
 INSERT INTO
     Profesional(
